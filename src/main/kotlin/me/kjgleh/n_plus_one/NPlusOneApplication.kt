ce64e9c5299175e0d8851b0dc8ec15ffcc3cb1d3
김정근 <gogoogjg@tripstore.kr>
2020-11-02 18:27:23 +0900

    initialize project

---
+++ src/main/kotlin/me/kjgleh/n_plus_one/NPlusOneApplication.kt
@@ -0,0 +1,11 @@
+package me.kjgleh.n_plus_one
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NPlusOneApplication
+
+fun main(args: Array<String>) {
+    runApplication<NPlusOneApplication>(*args)
+}
